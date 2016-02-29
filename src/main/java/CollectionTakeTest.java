430f3df10001c5db56a0fcf5152f744414e0dd1b
codedrinker <gwangchunlei@gmail.com>
2016-02-29 19:53:28 +0800

    first commit

---
+++ src/main/java/CollectionTakeTest.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by clwang on 2/2/16.
+ */
+public class CollectionTakeTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(4);
+        list.add(4);
+        list.add(4);
+
+        System.out.println(list);
+        System.out.println(list.size());
+        if (list.size() >= 6) {
+            List<Integer> integers = list.subList(0, 6);
+            System.out.println(list);
+            System.out.println(integers);
+        }
+
+    }
+}
---
+++ src/main/java/HashCodeTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by codedrinker on 2/25/16.
+ */
+public class HashCodeTest {
+    public static void main(String[] args) {
+        String a = "ab";
+        String b = "AB";
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+    }
+}
