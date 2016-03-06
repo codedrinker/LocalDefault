370a39f61168ed51400c8d5545c53a870f04531c
codedrinker <gwangchunlei@gmail.com>
2016-03-06 12:03:10 +0800

    exception and joiner test

---
+++ src/main/java/local/collection/JoinerTest.java
@@ -0,0 +1,26 @@
+package local.collection;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+
+/**
+ * Created by codedrinker on 3/4/16.
+ */
+public class JoinerTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = Lists.newArrayList(1, 2, 3, 4);
+        System.out.println(integers);
+        Joiner on = Joiner.on(",");
+        System.out.println(on);
+        String join = on.join(integers);
+        System.out.println(join);
+
+        Iterable<String> split = Splitter.on(",").split(join);
+        System.out.println(split);
+
+
+    }
+}
---
+++ src/main/java/local/exception/ExceptionTest.java
@@ -0,0 +1,28 @@
+package local.exception;
+
+import java.util.ArrayList;
+
+/**
+ * Created by codedrinker on 3/3/16.
+ */
+public class ExceptionTest {
+    public static void main(String[] args) {
+        ArrayList<String> objects = new ArrayList<String>();
+        objects.add("1");
+
+        System.out.println("call" + getExce(objects));
+    }
+
+    private static String getExce(ArrayList<String> objects) {
+        try {
+            System.out.println("try");
+            objects.get(2);
+            return "try";
+        } catch (Exception e) {
+            System.out.println("catch");
+        }
+
+        System.out.println("end");
+        return "end";
+    }
+}
