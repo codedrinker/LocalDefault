d28e3ea75fd22b1bdbf7e6865786cf25115b9367
codedrinker <gwangchunlei@gmail.com>
2016-04-25 20:43:36 +0800

    add sub list and regex test

---
+++ src/main/java/local/arraylist/ArrayListSublistTest.java
@@ -0,0 +1,36 @@
+package local.arraylist;
+
+import com.google.common.collect.Lists;
+
+import java.nio.channels.Pipe;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Created by codedrinker on 4/12/16.
+ */
+public class ArrayListSublistTest {
+    public static void main(String[] args) {
+        List<String> objects = Lists.newArrayList();
+        objects.add("1");
+//        objects.add("2");
+//        objects.add("3");
+//        objects.add("4");
+//        objects.add("5");
+//        objects.add("6");
+//        objects.add("7");
+        System.out.println(objects);
+        System.out.println(4+2+""+1);
+
+        if (objects.size() > 6) {
+            objects = objects.subList(0, 6);
+        } else if (objects.size() > 3) {
+            objects = objects.subList(0, 3);
+        } else {
+            objects = Lists.newArrayList();
+        }
+
+        System.out.println(objects);
+    }
+}
---
+++ src/main/java/local/btree/SubBinaryTree.java
@@ -0,0 +1,52 @@
+package local.btree;
+
+/**
+ * Created by codedrinker on 4/21/16.
+ */
+public class SubBinaryTree {
+    public static int isSubTree(TNode root1, TNode root2) {
+        if (root1 == null && root2 == null) {
+            return 1;
+        }
+        if (root2 == null) {
+            return -1;
+        }
+        if (root1 == null) {
+            return -1;
+        }
+
+        if (root1.value == root2.value) {
+            int left = isSubTree(root1.left, root2.left);
+            int right = isSubTree(root1.right, root2.right);
+            return left == 1 && right == 1 ? 1 : -1;
+        } else {
+            int left = isSubTree(root1.left, root2);
+            int right = isSubTree(root1.right, root2);
+            return right == 1 || left == 1 ? 1 : -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        TNode root3 = new TNode();
+        root3.value = 2;
+
+        TNode root2 = new TNode();
+        root2.value = 1;
+        root2.left = root3;
+
+        TNode root1 = new TNode();
+        root1.value = 1;
+        root1.left = root2;
+
+
+
+        System.out.println(isSubTree(root1, root2));
+    }
+}
+
+class TNode {
+    public int value;
+    public TNode left;
+    public TNode right;
+}
