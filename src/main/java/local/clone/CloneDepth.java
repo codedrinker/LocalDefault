c266f96fc804f49f4b8d58f5235c333f802bc767
codedrinker <gwangchunlei@gmail.com>
2016-10-16 10:17:25 +0800

    add clone

---
+++ src/main/java/local/clone/CloneDepth.java
@@ -0,0 +1,89 @@
+package local.clone;
+
+import local.collection.CollectionSortTest;
+
+/**
+ * Created by codedrinker on 10/16/16.
+ */
+public class CloneDepth {
+    public static void main(String[] args) {
+        Entity entity = new Entity();
+        entity.setId("userid");
+        entity.setInnerClass(new InnerClass("inner class origin"));
+        System.out.println(entity.getId());
+        System.out.println(entity.getInnerClass().getId());
+        Entity clone = entity.clone();
+        clone.setId("cloneId");
+        clone.getInnerClass().setId("clone inner class");
+        System.out.println(entity.getId());
+        System.out.println(entity.getInnerClass().getId());
+    }
+}
+
+class InnerClass implements Cloneable{
+    public InnerClass(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
+
+    public InnerClass clone() {
+        try {
+            return (InnerClass) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+class Entity implements Cloneable {
+    public InnerClass getInnerClass() {
+        return innerClass;
+    }
+
+    public void setInnerClass(InnerClass innerClass) {
+        this.innerClass = innerClass;
+    }
+
+    public Entity() {
+    }
+
+    public Entity(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
+    private InnerClass innerClass;
+
+
+    public Entity clone() {
+        try {
+            Entity clone = (Entity) super.clone();
+//            clone.setInnerClass(this.innerClass.clone());
+            return clone;
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
