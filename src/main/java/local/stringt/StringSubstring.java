dbe50e7340fd96008c2523998dba20f188d91cec
codedrinker <gwangchunlei@gmail.com>
2016-11-29 21:39:04 +0800

    fix

---
+++ src/main/java/local/stringt/StringSubstring.java
@@ -0,0 +1,13 @@
+package local.stringt;
+
+/**
+ * Created by codedrinker on 11/29/16.
+ */
+public class StringSubstring {
+    public static void main(String[] args) {
+        String a = "12345678";
+        System.out.println(a);
+        String substring = a.substring(0, 6);
+        System.out.println(substring);
+    }
+}
