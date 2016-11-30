a3a3481e089f9aa8afa5384ecd827d534a179cd6
codedrinker <gwangchunlei@gmail.com>
2016-11-30 10:56:45 +0800

    null boolean test

---
+++ src/main/java/local/booleant/BooleanValueOfTest.java
@@ -0,0 +1,18 @@
+package local.booleant;
+
+import org.apache.commons.lang.BooleanUtils;
+
+/**
+ * Created by codedrinker on 11/30/16.
+ */
+public class BooleanValueOfTest {
+    public static void main(String[] args) {
+        Boolean b = null;
+        System.out.println(b);
+
+        boolean aTrue = BooleanUtils.isTrue(b);
+        System.out.println(aTrue);
+
+
+    }
+}
