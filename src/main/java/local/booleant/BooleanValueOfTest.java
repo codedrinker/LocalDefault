a0dfb07e59d312e441b59c5104231317ec4dc2e1
codedrinker <gwangchunlei@gmail.com>
2016-12-26 11:20:29 +0800

    add boolean test

--- src/main/java/local/booleant/BooleanValueOfTest.java
+++ src/main/java/local/booleant/BooleanValueOfTest.java
@@ -10,5 +10,21 @@ public class BooleanValueOfTest {
         System.out.println(BooleanUtils.isTrue(null));
         System.out.println(BooleanUtils.isTrue(false));
         System.out.println(BooleanUtils.isTrue(true));
+        System.out.println();
+
+        System.out.println(BooleanUtils.isFalse(true));
+        System.out.println(BooleanUtils.isFalse(false));
+        System.out.println(BooleanUtils.isFalse(null));
+        System.out.println();
+
+        System.out.println(BooleanUtils.isNotTrue(true));
+        System.out.println(BooleanUtils.isNotTrue(false));
+        System.out.println(BooleanUtils.isNotTrue(null));
+        System.out.println();
+
+        System.out.println(BooleanUtils.isNotFalse(true));
+        System.out.println(BooleanUtils.isNotFalse(false));
+        System.out.println(BooleanUtils.isNotFalse(null));
+
     }
 }
