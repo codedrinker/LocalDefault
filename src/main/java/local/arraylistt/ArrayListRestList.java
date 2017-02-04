88234167d44ae0f0e638e0efa04e1e2d3a79fa63
codedrinker <gwangchunlei@gmail.com>
2017-02-04 10:00:25 +0800

    add rest list test

---
+++ src/main/java/local/arraylistt/ArrayListRestList.java
@@ -0,0 +1,20 @@
+package local.arraylistt;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by codedrinker on 02/02/2017.
+ */
+public class ArrayListRestList {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        Lists.asList()
+    }
+}
