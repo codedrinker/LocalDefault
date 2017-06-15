388123f108ea0689236191c95205c9c56df49fa0
codedrinker <gwangchunlei@gmail.com>
2017-06-15 20:00:08 +0800

    add date format pattern website

--- src/main/java/local/datet/JodaTimeDateFommetterTest.java
+++ src/main/java/local/datet/JodaTimeDateFommetterTest.java
@@ -9,6 +9,7 @@ import org.joda.time.format.DateTimeFormatter;
  * Created by codedrinker on 7/15/16.
  */
 public class JodaTimeDateFommetterTest {
+    //http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
     public static void main(String[] args) {
         String date = "Sat Apr 02 14:42:59 +0800 2011";
 //        String date = "2016-07-15T02:34:27+0000";//yyyy-MM-dd'T'HH:mm:ssZ
