9a19bc4617b74327a5dba0b4292e8d2e82aa1a89
codedrinker <gwangchunlei@gmail.com>
2017-11-19 12:32:28 +0800

    add array ref and main class test

---
+++ src/main/java/local/maint/MainTest.java
@@ -0,0 +1,14 @@
+package local.maint;
+
+/**
+ * Created by codedrinker on 19/11/2017.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        String s1 = args[1];
+        String s2 = args[2];
+        String s3 = args[3];
+        String s4 = args[4];
+        System.out.println("s2=" + s2);
+    }
+}
